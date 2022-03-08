b317695fc253e35e0c7131b3257817c42bc4e66e
Shabuddin <sfsaiyanfaraz@gmail.com>
2022-03-08 22:00:44 +0530

    UC1/MaximumNum

--- MaxNumber.java
+++ MaxNumber.java
@@ -4,12 +4,12 @@ public class MaxNumber{
 
 		Integer x,y,z;
 
-		public MaxNumber(Integer x, Integer y, Integer z) {
+		public MaxNumber(Integer x, Integer y, Integer z){
 			this.x = x;
 			this.y = y;
 			this.z = z;
 		}
-		public void print() {
+		public void print(){
 			System.out.println("Given Integer's are : ");
 			System.out.print(x +" ");
 			System.out.print(y +" ");
@@ -17,17 +17,17 @@ public class MaxNumber{
 			System.out.println(" ");
 		}
 		public void findMax() {
-			if (x.compareTo(y) > 0 && x.compareTo(z) > 0) {
+			if (x.compareTo(y) > 0 && x.compareTo(z) > 0){
 				System.out.println("Max Number is :"+x);
 
-			} else if (y.compareTo(x) > 0 && y.compareTo(z) > 0) {
+			} else if (y.compareTo(x) > 0 && y.compareTo(z) > 0){
 				System.out.println("Max Number is :"+y);
 
 			} else {
 				System.out.println("Max Number is :"+z);
 			}
 		}
-		public static void main(String[] args) {
+		public static void main(String[] args){
 			MaxNumber maxNum = new MaxNumber(1, 2, 3);
 			maxNum.print();
 			maxNum.findMax();
